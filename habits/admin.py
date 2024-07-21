6a8a8f3823eb657ae6e6667bd75abe3f1174a08e
ro_k_ <ro_k_sana@mail.ru>
2024-07-21 22:41:03 +0500

    CR_7_DRF. Админка app habits

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from habits.models import Action, Habit
 
-# Register your models here.
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'type_action', 'user',)
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'action', 'is_pleasant', 'link_action', 'award', 'space', 'time', 'periodicity', 'runtime',
+        'is_public',)
