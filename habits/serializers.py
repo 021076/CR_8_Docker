21ccca307c1153d90598a6711a3975500b676678
ro_k_ <ro_k_sana@mail.ru>
2024-07-21 22:42:28 +0500

    CR_7_DRF. habits: вью, эндпоинты, серилазиаторы, пагинация, валидаторы

---
+++ habits/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from habits.models import Action, Habit
+from habits.validators import MutuallyExclusiveFieldsValidator, PeriodicityValidator, RunTimeValidator
+
+
+# , PeriodicityValidator)
+# RunTimeValidator,
+
+class ActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Action
+        fields = '__all__'
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = '__all__'
+        validators = [MutuallyExclusiveFieldsValidator(), RunTimeValidator(), PeriodicityValidator()]
